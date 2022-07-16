06dc5bee9cd46ee37133da78dbbf98cf29dc11ff
charls96 <charly.lopez.perez@gmail.com>
2022-07-16 15:14:36 +0200

    custom scroll bar

--- src/components/page/Home.tsx
+++ src/components/page/Home.tsx
@@ -1,7 +1,20 @@
 const Home = () => {
   return (
-    <div className="bg-slate-900 overflow-y h-[80vh] max-h-[80vh] text-gray-300">Home</div>
-  )
-}
+    <div className="bg-slate-900 h-[80vh] overflow-y-auto overflow-x-hidden text-gray-300">
+        <p className="text-9xl">Home</p>
+        <p className="text-9xl">Home</p>
+        <p className="text-9xl">Home</p>
+        <p className="text-9xl">Home</p>
+        <p className="text-9xl">Home</p>
+        <p className="text-9xl">Home</p>
+        <p className="text-9xl">Home</p>
+        <p className="text-9xl">Home</p>
+        <p className="text-9xl">Home</p>
+        <p className="text-9xl">Home</p>
+        <p className="text-9xl">Home</p>
+        <p className="text-9xl">Home</p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
